c54d00e2d5c0911a2923739aead818408b3096a7
Ivan Kirshak <i.kirshak@gmail.com>
2020-04-27 17:57:58 +0300

    first commit. created header & footer

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require("browser-sync").create();
+const imagemin = require('gulp-imagemin');
+
+// SCSS compile (Gulp 4 syntax)
+sass.compiler = require('node-sass');
+function style() {
+    return gulp.src('./src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/css'))
+        .pipe(browserSync.stream());
+}
+
+// copy HTML
+function html () {
+    return gulp.src("src/*.html")
+        .pipe(gulp.dest('build'));
+}
+
+function images () {
+    return gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/images'))
+}
+
+function watch () {
+    browserSync.init({
+        server: {
+            baseDir: './build'
+        }
+    })
+    gulp.watch('./src/scss/**/*.scss', gulp.style).on('change', browserSync.reload);
+    gulp.watch('src/*.html', gulp.html).on('change', browserSync.reload);
+    gulp.watch('build/*.html').on('change', browserSync.reload);
+}
+
+//line below makes function above run when called
+exports.style = style;
+exports.html = html;
+exports.watch = watch;
+exports.images = images;
